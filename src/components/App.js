97ba8a97cedcb98fedaa6070194acdfe475de769
member <notebookmember@gmail.com>
2024-01-30 00:30:13 +0530

    Footer done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import Signup from "../components/Signup/Signup";
-import Login from "../components/Login/Login";
+// import Login from "../components/Login/Login";
+import Footer from "../components/Footer/Footer";
 
 function App() {
   return (
     <div>
       {/* <Signup /> */}
-      <Login />
+      {/* <Login /> */}
+      <Footer />
     </div>
   );
 }
